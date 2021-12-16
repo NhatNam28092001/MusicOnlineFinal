d8bbab86742d6efcbbc2fef4fc1d7c957c36d19c
NhatNam28092001 <nhatnamd14cnpm8@gmail.com>
2021-12-16 16:01:11 +0700

    update blog,casi,sanpham

---
+++ OnlineMusic/DAO/BLOG_DAO.cs
@@ -0,0 +1,33 @@
+﻿using OnlineMusic.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineMusic.DAO
+{
+    public class BLOG_DAO
+    {
+        OnlineMusicDB db = null;
+        public BLOG_DAO()
+        {
+            db = new OnlineMusicDB();
+        }
+        public IEnumerable<NEWS> ListAllPaging(int page, int pageSize)
+        {
+
+            return db.NEWS.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+        public bool Insert(NEWS entity)
+        {
+            db.NEWS.Add(entity);
+            db.SaveChanges();
+            return true;
+        }
+        public List<NEWS> ListAll()
+        {
+            return db.NEWS.OrderBy(x => x.ID).ToList();
+        }
+    }
+}
---
+++ OnlineMusic/DAO/PRODUCT_DAO.cs
@@ -0,0 +1,32 @@
+﻿using OnlineMusic.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineMusic.DAO
+{
+    public class PRODUCT_DAO
+    {
+        OnlineMusicDB db = null;
+        public PRODUCT_DAO()
+        {
+            db = new OnlineMusicDB();
+        }
+        public List<SANPHAM> ListAll()
+        {
+            return db.SANPHAMs.Where(x => x.Status == true).OrderBy(x => x.ID).ToList();
+        }
+        public bool Insert(SANPHAM entity)
+        {
+            db.SANPHAMs.Add(entity);
+            db.SaveChanges();
+            return true;
+        }
+        public IEnumerable<SANPHAM> ListAllPaging(int page, int pageSize)
+        {
+            return db.SANPHAMs.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+    }
+}
